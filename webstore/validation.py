7e902e1cc9c1d07c7972df423db9c774061ac68f
Ross Jones <rossdjones@gmail.com>
2012-01-12 15:08:33 +0000

    Fix the allowed characters in table names, - should be allowed.

--- webstore/validation.py
+++ webstore/validation.py
@@ -5,7 +5,7 @@ class NamingException(Exception):
     def __init__(self, field):
         self.field = field
 
-VALID_NAME = re.compile('^[a-zA-Z0-9][a-zA-Z0-9_]{0,254}$')
+VALID_NAME = re.compile('^[a-zA-Z0-9\-][a-zA-Z0-9_\-]{0,254}$')
 VALID_DBNAME = re.compile('^[a-zA-Z0-9][a-zA-Z0-9_\-]{0,254}$')
 VALID_USERNAME = re.compile('^[a-zA-Z0-9][a-zA-Z0-9_\-\.]{0,254}$')
 
